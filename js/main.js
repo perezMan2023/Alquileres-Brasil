14f651d66d5b5b60c716bc9aa740f78a6a19b395
doalperez12@gmail.com <doalperez12@gmail.com>
2023-10-05 09:56:35 -0300

    segundo arreglos

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 function initializeSlider(_slider-frame-., options) {
-    // Obtener el contenedor del slider
+  
     const slider-frame = document.getElementById(containerId);
     
-    // Lógica para inicializar el slider usando la librería o código que prefieras
-    // Por ejemplo, aquí asumimos que estás utilizando una librería como Slick Slider
+   
     $(slider-frame).slick(options);
   }
-  // Inicializar un slider en el contenedor "slider-container-1"
-  initializeSlider("slider-container-1", {
+  
+  initializeSlider("slider-frame", {
     slidesToShow: 3,
     autoplay: true,
-    // Otras opciones
   });
   
-  // Inicializar otro slider en el contenedor "slider-container-2"
-  initializeSlider("slider-container-2", {
-    slidesToShow: 2,
-    autoplay: false,
-    // Otras opciones
-  });
+/*  La idea es utilizar este codigo para no hacer SLIDER en cada publicidad (re-utilizar codigo)
+estoy pensando que cada publicidad tiene distintas fotos entonces NO SE SI SIRVE ESTO*/
   
